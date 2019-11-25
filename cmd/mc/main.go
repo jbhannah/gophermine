f964144aa369c5341f7a2609b90446e42fc4b954
Jesse B. Hannah <jesse@jbhannah.net>
2019-11-24 17:35:26 -0700

    Use pflag for CLI flag parsing

--- cmd/mc/main.go
+++ cmd/mc/main.go
@@ -11,32 +11,63 @@ import (
 
 	"github.com/jbhannah/gophermine/internal/pkg/server"
 	log "github.com/sirupsen/logrus"
-	"github.com/urfave/cli/v2"
+	flag "github.com/spf13/pflag"
 )
+
+// Version is the Gophermine release version
+const Version = "v0.0.1"
+
+// MCVersion is the Minecraft™: Java Edition version that this release of
+// Gophermine is compatible with.
+const MCVersion = "1.14.4"
+
+// MCProtocolVersion is the Minecraft protocol version number that this release
+// of Gophermine is compatible with.
+const MCProtocolVersion = 498
+
+var help bool
+var verbose bool
+var version bool
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: "2006-01-02T15:04:05.000000Z-07:00",
 	})
-	log.SetLevel(log.DebugLevel)
 }
 
 func main() {
-	app := &cli.App{
-		Name:    "Gophermine",
-		Usage:   "A (someday) Minecraft™: Java Edition compatible server written in Go.",
-		Version: "0.0.1",
-		Action:  start,
+	flag.BoolVarP(&help, "help", "h", false, "show this help message")
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+	flag.BoolVarP(&version, "version", "v", false, "print the version")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	if help {
+		printVersion()
+		fmt.Fprintln(os.Stderr)
+		flag.Usage()
+		os.Exit(0)
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	if version {
+		printVersion()
+		os.Exit(0)
+	}
+
+	if err := start(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func start(c *cli.Context) error {
+func start() error {
 	log.Info("Starting Gophermine")
+
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("Enabled verbose logging")
+	}
 
 	config, err := mc.NewServerConfig()
 	if err != nil {
@@ -72,4 +103,13 @@ func handleSigs(cancel context.CancelFunc, sigs <-chan os.Signal) {
 	print("\r")
 	log.Warn(fmt.Sprintf("Received %s signal", sig))
 	log.Warn("Stopping Gophermine")
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage of mc:")
+	flag.PrintDefaults()
+}
+
+func printVersion() {
+	fmt.Fprintf(os.Stderr, "Gophermine %s (Java Edition version %s, protocol %d)\n", Version, MCVersion, MCProtocolVersion)
 }
